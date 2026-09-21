55fa96200331f7f70e2b82102f1f1209ae7e5278
nishantp1997 <35177295+nishantp1997@users.noreply.github.com>
2026-09-21 19:43:11 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.court.filetracker.pro"
+        applicationId = "com.court.tracker.pro"
         minSdk = 24
         targetSdk = 34
         versionCode = 2
